6be6ea7783c8a3a3bebc79628e1138d7d424bb6a
Hauke <me@hauke-witte.de>
2019-05-20 02:11:15 +0200

    added production build to backend

--- backend/graphql/dbconfig.js
+++
@@ -1,7 +0,0 @@
-const IS_DEV = process.env.NODE_ENV === "dev";
-
-const database = IS_DEV ? "tasklist-dev" : "tasklist-prod";
-
-export default {
-  uri: `mongodb+srv://Juviro:HD35twuZuQiAm6y@cluster0-lybnc.mongodb.net/${database}?retryWrites=true`,
-};
